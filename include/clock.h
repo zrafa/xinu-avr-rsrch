ae115a50635264d59ae7e2f1dd28ede1f9e2ba1e
Rodolfo del Castillo <rdc541@yahoo.com.ar>
2020-11-30 18:40:17 -0300

    mas podada ...

--- include/clock.h
+++ include/clock.h
@@ -5,6 +5,5 @@ extern  volatile uint32  count100;      /* 10 ms since last clock tick          
 
 extern	qid16	sleepq;		/* queue for sleeping processes		*/
 extern	int32	slnonempty;	/* nonzero if sleepq is nonempty	*/
-extern	int32	*sltop;		/* ptr to key in first item on sleepq	*/
 extern	uint32	preempt;	/* preemption counter			*/
 
